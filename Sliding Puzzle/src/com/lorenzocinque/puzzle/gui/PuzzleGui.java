593e07be126237e5a6cf4dcfe3257d1774c7f085
Mulder90 <lore.cinque@gmail.com>
2013-05-18 22:45:49 +0200

    Improved GUI

--- Sliding Puzzle/src/com/lorenzocinque/puzzle/gui/PuzzleGui.java
+++ Sliding Puzzle/src/com/lorenzocinque/puzzle/gui/PuzzleGui.java
@@ -1,8 +1,10 @@
 package com.lorenzocinque.puzzle.gui;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -11,19 +13,29 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.lorenzocinque.puzzle.core.Puzzle;
+import com.lorenzocinque.puzzle.core.PuzzleGoalTest;
+import com.lorenzocinque.puzzle.core.PuzzleState;
+import com.lorenzocinque.puzzle.core.Solution;
+import com.lorenzocinque.puzzle.core.Solver;
+import com.lorenzocinque.puzzle.core.Utils;
+import com.lorenzocinque.puzzle.core.heuristic.ManhattanHeuristic;
+import com.lorenzocinque.puzzle.core.searchalgorithm.AStar;
 
 public class PuzzleGui {
 
 	private JFrame frameSlidingPuzzle;
+	private JTextArea textArea;
 	private JTextField seedTextField;
 	private JTextField scramblesTextField;
-	private int N;
-	private Puzzle puzzle; 
+	private int N = 3;
+	private Puzzle puzzle;
 
 	/**
 	 * Create the application.
@@ -38,6 +50,7 @@ public class PuzzleGui {
 
 	public void setFrameSlidingPuzzle(JFrame frameSlidingPuzzle) {
 		this.frameSlidingPuzzle = frameSlidingPuzzle;
+		frameSlidingPuzzle.setResizable(false);
 	}
 
 	/**
@@ -53,54 +66,95 @@ public class PuzzleGui {
 						BoxLayout.Y_AXIS));
 
 		JPanel panel = new JPanel();
+		panel.setMaximumSize(new Dimension(32767, 50));
 		getFrameSlidingPuzzle().getContentPane().add(panel);
-		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
-		JComboBox dimensionComboBox = new JComboBox();
-		dimensionComboBox.addItemListener(new ItemListener() {
-			
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				N = Integer.parseInt(e.getItem().toString());
-				System.out.println(N);
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		getFrameSlidingPuzzle().getContentPane().add(scrollPane);
+		panel.setLayout(new GridLayout(2, 1, 0, 0));
+
+		JPanel creationPanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) creationPanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panel.add(creationPanel);
+
+		JPanel algorithmPanel = new JPanel();
+		flowLayout = (FlowLayout) algorithmPanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panel.add(algorithmPanel);
+
+		final JComboBox<String> dimensionComboBox = new JComboBox<String>();
+		creationPanel.add(dimensionComboBox);
+		dimensionComboBox.setModel(new DefaultComboBoxModel<String>(
+				new String[] { "8 Puzzle", "15 Puzzle" }));
+
+		dimensionComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int i = dimensionComboBox.getSelectedIndex();
+				N = (i == 0) ? 3 : 4;
 			}
 		});
-		
-		dimensionComboBox.setModel(new DefaultComboBoxModel(new String[] { "3",
-				"4" }));
-		panel.add(dimensionComboBox);
 
-		JComboBox algorithmComboBox = new JComboBox();
-		algorithmComboBox.setModel(new DefaultComboBoxModel(new String[] {
-				"A*", "IDA*", "Breadth First Search" }));
-		panel.add(algorithmComboBox);
-
-		JComboBox heuristicComboBox = new JComboBox();
-		heuristicComboBox.setModel(new DefaultComboBoxModel(new String[] {
-				"Manhattan", "Misplaced" }));
-		panel.add(heuristicComboBox);
-
+		JLabel seedLabel = new JLabel("Seed:");
+		creationPanel.add(seedLabel);
 		seedTextField = new JTextField();
-		seedTextField.setText("8596");
-		panel.add(seedTextField);
+		creationPanel.add(seedTextField);
+		seedTextField.setText("1200");
 		seedTextField.setColumns(10);
 
+		JLabel scramblesLabel = new JLabel("Scrambles:");
+		creationPanel.add(scramblesLabel);
 		scramblesTextField = new JTextField();
+		creationPanel.add(scramblesTextField);
 		scramblesTextField.setText("60");
-		panel.add(scramblesTextField);
-		scramblesTextField.setColumns(10);
+		scramblesTextField.setColumns(3);
 
-		JButton buttonSolve = new JButton("Solve");
-		buttonSolve.addMouseListener(new MouseAdapter() {
+		final JComboBox<String> algorithmComboBox = new JComboBox<String>();
+		algorithmPanel.add(algorithmComboBox);
+		algorithmComboBox.setModel(new DefaultComboBoxModel<String>(
+				new String[] { "A*", "IDA*", "Breadth First Search" }));
+
+		final JComboBox<String> heuristicComboBox = new JComboBox<String>();
+		algorithmPanel.add(heuristicComboBox);
+		heuristicComboBox.setModel(new DefaultComboBoxModel<String>(
+				new String[] { "Manhattan", "Misplaced" }));
+
+		JButton createButton = new JButton("Create");
+		creationPanel.add(createButton);
+
+		createButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				textArea.setText("");
+				puzzle = new Puzzle(N, new PuzzleState(N),
+						new PuzzleGoalTest(N));
+				int seed = Integer.parseInt(seedTextField.getText());
+				int scrambles = Integer.parseInt(scramblesTextField.getText());
+				Utils.makeRandomInitialState(puzzle, seed, scrambles);
+				textArea.append("\nInitial state:\n" + puzzle.getInitialState()
+						+ "\n");
 			}
 		});
-		panel.add(buttonSolve);
 
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		getFrameSlidingPuzzle().getContentPane().add(textArea);
+		JButton solveButton = new JButton("Solve");
+		algorithmPanel.add(solveButton);
+
+		solveButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Solver solver = new Solver(puzzle, new AStar(
+						new ManhattanHeuristic(N)));
+				long startTime = System.nanoTime();
+				Solution solution = solver.solve();
+				long searchTime = System.nanoTime() - startTime;
+				textArea.append(solution.toString() + "\n");
+				textArea.append("Search finished with " + solver.toString()
+						+ " in approximately: " + (searchTime / 1000000000.0)
+						+ "s");
+			}
+		});
 	}
 
 }
